33fc9eb760d3730c98331a0d691a1ba2101145db
Thein Naing <theinnaing0908@gmail.com>
2023-08-21 14:48:48 +0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,75 +40,78 @@ const Login = () => {
 
   return (
     <Container
-       style={{
-          height: "100vh",
-          backgroundColor:" black",
+      style={{
+        height: "100vh",
+        backgroundColor: " black",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      {/* <Link to="/"></Link> */}
+      <div
+        style={{
+          width: "300px",
+          height: "fit-content",
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
-
-      }}>
-        {/* <Link to="/"></Link> */}
-        <div
-          style={{
-            width: "300px",
-            height: "fit-content",
-            display: "flex",
-            flexDirection: "column",
-            borderRadius: "5px",
-            border: "1px solid lightgray",
-            padding: "20px",
-            marginTop:"100px"
-          }}
-        >
-          <h3>Sign-in</h3>
-          <form>
-            <h5 style={{ mb: "5px"}}>E-mail</h5>
-            <input
-              style={{height: "30px",
+          borderRadius: "5px",
+          border: "1px solid lightgray",
+          padding: "20px",
+          marginTop: "100px",
+        }}
+      >
+        <h3>Sign-in</h3>
+        <form>
+          <h5 style={{ mb: "5px" }}>E-mail</h5>
+          <input
+            style={{
+              height: "30px",
               mb: "10px",
               backgroundColor: "white",
-              width: "100%"}}
-
-              type="text"
-              value={email}s
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            <h5 style={{ mb: "5px"}}>Password</h5>
-            <input
-              style={{height: "30px",
+              width: "100%",
+            }}
+            type="text"
+            value={email}
+            s
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <h5 style={{ mb: "5px" }}>Password</h5>
+          <input
+            style={{
+              height: "30px",
               mb: "10px",
               backgroundColor: "white",
-              width: "100%"}}
-              type="password"
-              value={password}
-              onChange={(e) => {
+              width: "100%",
+            }}
+            type="password"
+            value={password}
+            onChange={(e) => {
               setPassword(e.target.value);
-
-              }}
-            />
-            <button
-              onClick={signIn}
-              type="submit"
-              style={{
-                background: "blueviolet",
-                borderRadius: "2px",
-                width: "100%",
-                height: "30px",
-                border: "1px solid",
-                mt: "20px",
-                borderColor: "#5b34a8 #5b34a7 #5b34a6"
-              }}
-            >
-              Sign In
-            </button>
-            <p>
-              By signing-in you agree to Movieland's Terms and Conditions of Use
-              & Sale.
-            </p>
-            <button
+            }}
+          />
+          <button
+            onClick={signIn}
+            type="submit"
+            style={{
+              background: "blueviolet",
+              borderRadius: "2px",
+              width: "100%",
+              height: "30px",
+              border: "1px solid",
+              mt: "20px",
+              borderColor: "#5b34a8 #5b34a7 #5b34a6",
+            }}
+          >
+            Sign In
+          </button>
+          <p style={{ mt: "15px", fontSize: "12px" }}>
+            By signing-in you agree to Movieland's Terms and Conditions of Use &
+            Sale.
+          </p>
+          <button
             onClick={signUp}
             style={{
               background: "blueviolet",
@@ -117,13 +120,13 @@ const Login = () => {
               height: "30px",
               border: "1px solid",
               mt: "20px",
-              borderColor: "#5b34a8 #5b34a7 #5b34a6"
-            }}>
-              Register
-            </button>
-          </form>
-        </div>
-
+              borderColor: "#5b34a8 #5b34a7 #5b34a6",
+            }}
+          >
+            Register
+          </button>
+        </form>
+      </div>
     </Container>
   );
 };
